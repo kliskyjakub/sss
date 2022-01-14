5acb5210829af416aba29da6ecf8bd60791dd092
Yi Hein Chai <chaiyihein@gmail.com>
2022-01-14 18:40:51 +0000

    added simple buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [walletConnected, setWalletConnected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setWalletConnected(true)}>Connect wallet</button>
+      {walletConnected && <div>Video is here!</div>}
     </div>
   );
 }
